a0dd58c49b91096029b900f50a7d77b0fb020046
Bagus Dzikri Hidayat <bagusdzikri@gmail.com>
2019-11-09 21:29:03 +0700

    Tugas seleksi bootcamp no. 3 selesai dikerjakan

--- Soal3.js
+++ Soal3.js
@@ -1,1 +1,12 @@
-Soal Bootcamp No. 3
+//Soal Seleksi Bootcamp No. 3
+
+function thirdHighest(arr) {
+    if (arr.length >= 3) {
+        sortedArr = arr.sort(function(firstNum, secondNum) { return firstNum < secondNum })
+        return sortedArr[2]
+    } else if (arr.length < 3) {
+        return 'Minimal array length is 3!'
+    } else {
+        return 'Parameter should be an array!'
+    }
+}
